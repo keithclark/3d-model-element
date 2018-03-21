2212eddd5470886b5b71c90b82fca6faca3ae60b
Keith Clark <keith@keithclark.co.uk>
2018-03-21 18:18:55 +0000

    Hook up connected/disconnected custom-element callbacks

--- src/modelElement.js
+++ src/modelElement.js
@@ -2,6 +2,9 @@ import modelLoader from './modelLoader';
 import modelLayer from './modelLayer';
 
 
+const objects = new WeakMap();
+
+
 export default class extends HTMLElement {
 
   constructor() {
@@ -13,17 +16,31 @@ export default class extends HTMLElement {
   }
 
   connectedCallback() {
-
+    let obj = objects.get(this);
+    if (obj && obj.elem !== this) {
+      obj.elem = this;
+      modelLayer.add(obj);
+    }
   }
 
   disconnectedCallback() {
+    let obj = objects.get(this);
+    if (obj && obj.elem === this) {
+      modelLayer.remove(obj);
+      obj.elem = null;
+    }
   }
 
   attributeChangedCallback(attribute, oldValue, newValue) {
     if (attribute === 'src') {
+
+      // call the disconnected callback handler to release the current model if
+      // one is attached
+      this.disconnectedCallback();
+
       modelLoader.load(newValue).then(obj => {
-        obj.elem = this;
-        modelLayer.add(obj);
+        objects.set(this, obj);
+        this.connectedCallback();
       });
     }
   }
