865b3ff1288ab2f517ea90c549f3ae57c61ffdd4
Keith Clark <keith@keithclark.co.uk>
2019-05-14 00:33:49 +0100

    Remove unused debugging helpers

--- src/utils/model.js
+++ src/utils/model.js
@@ -1,8 +1,3 @@
-/* While mapping objects to a DOM element, it's useful to have a bounding box
-to reference. Enabling this flag will produce just that, a 2D bounding rect
-that should match the related DOM element */
-const RENDER_OBJECT_BOUNDING_BOX = false;
-
 
 const normalize = obj => {
   let box = new THREE.Box3().setFromObject(obj);
@@ -17,30 +12,14 @@ const normalize = obj => {
 };
 
 
-const createDebugBoundingBox = obj => {
-  let material = new THREE.LineBasicMaterial({color: 0x00ff00});
-  let geometry = new THREE.PlaneGeometry(1, 1);
-  let wireframe = new THREE.WireframeGeometry(geometry);
-  let boundingBox = new THREE.LineSegments(wireframe, material);
-  return boundingBox;
-};
-
-
 const createContainer = obj => {
   let container = new THREE.Object3D();
-
-  if (RENDER_OBJECT_BOUNDING_BOX) {
-    container.add(createDebugBoundingBox());
-  }
-
   container.add(obj);
-
   return container;
 };
 
 
 export default {
   normalize,
-  createDebugBoundingBox,
   createContainer
 };
